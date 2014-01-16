1c9c67ce5d9c3d705bbb22b3d97ab2eac9a3debd
Rachel Wang <rswang@mit.edu>
2014-01-16 16:00:49 -0500

    Fix programs nav

--- programs.php
+++ programs.php
@@ -5,37 +5,7 @@
 		<!--Navigation-->
 				
 		<?php include 'nav.php'; ?>
-				
-						
-		<nav>
-			<a id='logo' href='necysc.html'><img id='navlogo' src='images/logo2.png'></a>
-			<ul>
-				<li><a href='#about'>About Us</a>
-					<ul>
-						<li><a href='/history/'>History</a></li>
-						<li><a href='/team/'>Team</a></li>
-					</ul>
-				</li>
-				<li><a href='#programs'>Programs</a>
-					<ul>
-						<li><a href='/day/'>Day Camp</a></li>
-						<li><a href='/overnight/'>Overnight Camp</a></li>
-						<li><a href='/explorasian/'>ExplorAsian</a></li>
-						<li><a href='/staff'>Staff</a>
-							<ul>
-								<li><a href='/CIT/'>CIT</a></li>
-								<li><a href='/counselor/'>Counselor</a></li>
-								<li><a href='/RA/'>Residential Advisor</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li><a href='#faq/'>FAQ</a></li>
-				<li><a href='#contact/'>Contact</a></li>
-				<li><a href='#sponsor/'>Sponsors</a></li>
-				<li><a href='login.html'>Login</a></li>
-			</ul>
-		</nav>
+		
 		
 
 		<!--Main-->
