2d64337b38e590873fe4b885667ef6cf51dbee2c
Rachel Wang <rswang@mit.edu>
2014-01-18 14:01:17 -0500

    Add email form

--- email.php
+++ email.php
@@ -1,55 +1,97 @@
-<html>
-<body>
-<?php
-function spamcheck($field)
-  {
-  //filter_var() sanitizes the e-mail
-  //address using FILTER_SANITIZE_EMAIL
-  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
-
-  //filter_var() validates the e-mail
-  //address using FILTER_VALIDATE_EMAIL
-  if(filter_var($field, FILTER_VALIDATE_EMAIL))
-    {
-    return TRUE;
-    }
-  else
-    {
-    return FALSE;
-    }
-  }
-
-if (isset($_REQUEST['email']))
-  {//if "email" is filled out, proceed
-
-  //check if the email address is invalid
-  $mailcheck = spamcheck($_REQUEST['email']);
-  if ($mailcheck==FALSE)
-    {
-    echo "Invalid input";
-    }
-  else
-    {//send email
-    $email = $_REQUEST['email'] ;
-    $subject = $_REQUEST['subject'] ;
-    $message = $_REQUEST['message'] ;
-    mail("someone@example.com", "Subject: $subject",
-    $message, "From: $email" );
-    echo "Thank you for using our mail form";
-    }
-  }
-else
-  {//if "email" is not filled out, display the form
-  echo "<form method='post' action='mailform.php'>
-  Email: <input name='email' type='text'><br>
-  Subject: <input name='subject' type='text'><br>
-  Message:<br>
-  <textarea name='message' rows='15' cols='40'>
-  </textarea><br>
-  <input type='submit'>
-  </form>";
-  }
-?>
-
-</body>
-</html>
+<form name="contactform" method="post" action="mailform.php">
+ 
+<table width="450px">
+ 
+<tr>
+ 
+ <td valign="top">
+ 
+  <label for="first_name">First Name *</label>
+ 
+ </td>
+ 
+ <td valign="top">
+ 
+  <input  type="text" name="first_name" maxlength="50" size="30">
+ 
+ </td>
+ 
+</tr>
+ 
+<tr>
+ 
+ <td valign="top">
+ 
+  <label for="last_name">Last Name *</label>
+ 
+ </td>
+ 
+ <td valign="top">
+ 
+  <input  type="text" name="last_name" maxlength="50" size="30">
+ 
+ </td>
+ 
+</tr>
+ 
+<tr>
+ 
+ <td valign="top">
+ 
+  <label for="email">Email Address *</label>
+ 
+ </td>
+ 
+ <td valign="top">
+ 
+  <input  type="text" name="email" maxlength="80" size="30">
+ 
+ </td>
+ 
+</tr>
+ 
+<tr>
+ 
+ <td valign="top">
+ 
+  <label for="telephone">Telephone Number</label>
+ 
+ </td>
+ 
+ <td valign="top">
+ 
+  <input  type="text" name="telephone" maxlength="30" size="30">
+ 
+ </td>
+ 
+</tr>
+ 
+<tr>
+ 
+ <td valign="top">
+ 
+  <label for="comments">Comments *</label>
+ 
+ </td>
+ 
+ <td valign="top">
+ 
+  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+ 
+ </td>
+ 
+</tr>
+ 
+<tr>
+ 
+ <td colspan="2" style="text-align:center">
+ 
+  <input type="submit" value="Submit">   <a href="http://www.freecontactform.com/email_form.php">Email Form</a>
+ 
+ </td>
+ 
+</tr>
+ 
+</table>
+ 
+</form>
