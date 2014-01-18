3add1d6deb451c01237d1187488724d6f2b7f1a6
Rachel Wang <rswang@mit.edu>
2014-01-18 17:48:37 -0500

    Add camp fees page

---
+++ campfees.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include 'nav.php'; ?>
+		<title>NECYSC</title>
+	</head>
+	<body>
+		<!--Navigation-->
+				
+		<!-- <?php include 'nav.php'; ?>-->	
+		<nav>
+			<a id='logo' href='necysc.html'><img id='navlogo' src='images/logo2.png'></a>
+			<ul>
+				<li><a href='#home'>Home</a>
+					<ul>
+						<li><a href='#about'>About</a></li>
+						<li><a href='#sponsors'>Sponsors</a></li>
+						<li><a href='#contact'>Contact</a></li>
+						<li><a href='/team/'>Team</a></li>
+					</ul>
+				</li>
+				<li><a href='programs.html'>Programs</a>
+					<ul>
+						<li><a href='/day/'>Day Camp</a></li>
+						<li><a href='/overnight/'>Overnight Camp</a></li>
+						<li><a href='/explorasian/'>ExplorAsian</a></li>
+						<li><a href='/staff'>Staff</a>
+							<ul>
+								<li><a href='/CIT/'>CIT</a></li>
+								<li><a href='/counselor/'>Counselor</a></li>
+								<li><a href='/RA/'>Residential Advisor</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href='#faq/'>Campers & Parents</a>
+					<ul>
+						<li><a href=''>Forms</a></li>
+						<li><a href=''>Financial Aid</a></li>
+						<li><a href=''>Parents on Duty</a></li>
+					</ul>
+				</li>
+
+				<li><a href='faq.html'>FAQ</a></li>
+				<li><a href='login.html'>Login</a></li>
+			</ul>
+		</nav>
+		
+
+		<!--Main-->
+		<div class= "main">
+		<section id="fees">
+			<article>
+				<table class ='tprograms'><tr><td>
+					<div class='programcontainer'>
+				<table>
+					<tr>
+						<td>
+							<strong>Camp Program</strong>
+						</td>
+						<td>
+							<strong>2014 Fees</strong>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day Camp
+						</td>
+						<td>
+							$420
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Overnight Camp
+						</td>
+						<td>
+							$750
+						</td>
+					</tr>
+					<tr>
+						<td>
+							ExplorAsian
+						</td>
+						<td>
+							$750
+						</td>
+					</tr>	
+					<tr>
+						<td>
+							CIT
+						</td>
+						<td>
+							$420
+						</td>
+					</tr>
+				</table></div>
+			</td>
+			<td>
+				<div class="flexslider">
+					  <ul class="slides">
+					    <li>
+					      <img src="images/v1.JPG" />
+					    </li>
+					    <li>
+					      <img src="images/v2.JPG" />
+					    </li>
+					    <li>
+					      <img src="images/v3.JPG" />
+					    </li>
+					  </ul>
+					</div>
+				</td></tr></table>
+			</article>
+		</section>
+
+		<section id='financialaid'>
+			<article>
+		We try our best to make our programs affordable for everyone. Financial Aid is available and is based solely on need.  Families who qualify are encouraged to apply as soon as possible. It is essential that  you provide support document to demonstrate your need. Our goal is to offer an opportunity to families who may otherwise not be able to afford the program to share our camp experience.
+
+		The Financial Aid application is strictly confidential. Financial Aid is a separate process from program acceptance and is awarded after program acceptance. Please complete both the Camper/CIT program application form (indicating that you are applying for Financial Aid) and the Financial Aid application form. Please also note that it is required that you send in a 50% deposit with your financial application.
+	</article>
+	</section>
+</div>
+
+	</body>
+</html>
