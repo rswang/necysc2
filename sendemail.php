c44be8c7ccff6052998492914b851c5ee99c6466
Rachel Wang <rswang@mit.edu>
2014-01-18 14:43:45 -0500

    email

--- sendemail.php
+++ sendemail.php
@@ -9,7 +9,7 @@ if (isset($_REQUEST['email']))
   $email = $_REQUEST['email'] ;
   $subject = $_REQUEST['subject'] ;
   $message = $_REQUEST['message'] ;
-  mail("someone@example.com", $subject,
+  mail("rswang@mit.edu", $subject,
   $message, "From:" . $email);
   echo "Thank you for using our mail form";
   }
