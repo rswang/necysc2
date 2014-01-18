ebf503146421afaa29c639321ffcdaeb2d3b301a
Rachel Wang <rswang@mit.edu>
2014-01-18 13:50:20 -0500

    Email form

--- necysc.php
+++ necysc.php
@@ -154,6 +154,61 @@
 			</div>
 			<div id='texture'></div>
 
+
+
+<?php
+function spamcheck($field)
+  {
+  //filter_var() sanitizes the e-mail
+  //address using FILTER_SANITIZE_EMAIL
+  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+  //filter_var() validates the e-mail
+  //address using FILTER_VALIDATE_EMAIL
+  if(filter_var($field, FILTER_VALIDATE_EMAIL))
+    {
+    return TRUE;
+    }
+  else
+    {
+    return FALSE;
+    }
+  }
+
+if (isset($_REQUEST['email']))
+  {//if "email" is filled out, proceed
+
+  //check if the email address is invalid
+  $mailcheck = spamcheck($_REQUEST['email']);
+  if ($mailcheck==FALSE)
+    {
+    echo "Invalid input";
+    }
+  else
+    {//send email
+    $email = $_REQUEST['email'] ;
+    $name = $_REQUEST['name'] ;
+    $message = $_REQUEST['message'] ;
+    mail("rswang@mit.edu", "Subject: NECYSC Contact Us",
+    $email+ $name+ $message, "From: rachelwang1994@gmail.com" );
+    echo "Thank you for using our mail form";
+    }
+  }
+else
+  {//if "email" is not filled out, display the form
+  echo "<form method='post' action='mailform.php'>
+  Email: <input name='email' type='text'><br>
+  Name: <input name='name' type='text'><br>
+  Message:<br>
+  <textarea name='message' rows='15' cols='40'>
+  </textarea><br>
+  <input type='submit'>
+  </form>";
+  }
+?>
+
+
+
 	 </div>
 	</body>
 </html>
