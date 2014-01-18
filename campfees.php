eaef6fefc7bb3fb5f6d3953d64459ddfd385d30e
Rachel Wang <rswang@mit.edu>
2014-01-18 17:49:16 -0500

    fix php on camp fees page

--- campfees.php
+++ campfees.php
@@ -1,50 +1,14 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php include 'nav.php'; ?>
+		<?php include 'header.php'; ?>
 		<title>NECYSC</title>
 	</head>
 	<body>
 		<!--Navigation-->
 				
 		<!-- <?php include 'nav.php'; ?>-->	
-		<nav>
-			<a id='logo' href='necysc.html'><img id='navlogo' src='images/logo2.png'></a>
-			<ul>
-				<li><a href='#home'>Home</a>
-					<ul>
-						<li><a href='#about'>About</a></li>
-						<li><a href='#sponsors'>Sponsors</a></li>
-						<li><a href='#contact'>Contact</a></li>
-						<li><a href='/team/'>Team</a></li>
-					</ul>
-				</li>
-				<li><a href='programs.html'>Programs</a>
-					<ul>
-						<li><a href='/day/'>Day Camp</a></li>
-						<li><a href='/overnight/'>Overnight Camp</a></li>
-						<li><a href='/explorasian/'>ExplorAsian</a></li>
-						<li><a href='/staff'>Staff</a>
-							<ul>
-								<li><a href='/CIT/'>CIT</a></li>
-								<li><a href='/counselor/'>Counselor</a></li>
-								<li><a href='/RA/'>Residential Advisor</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li><a href='#faq/'>Campers & Parents</a>
-					<ul>
-						<li><a href=''>Forms</a></li>
-						<li><a href=''>Financial Aid</a></li>
-						<li><a href=''>Parents on Duty</a></li>
-					</ul>
-				</li>
-
-				<li><a href='faq.html'>FAQ</a></li>
-				<li><a href='login.html'>Login</a></li>
-			</ul>
-		</nav>
+		<?php include 'header.php'; ?>
 		
 
 		<!--Main-->
