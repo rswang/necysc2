3bc819b839955b81d885d891ed09d6b7e184221a
Rachel Wang <rswang@mit.edu>
2014-01-20 03:29:23 -0500

    Add parents page

---
+++ parents.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>NECYSC</title>
+	</head>
+
+	<?php include 'header.php'; ?>
+	<body>
+		<!--Navigation-->
+				
+		<?php include 'nav.php'; ?>
+
+
+		<!--Main-->
+		<div class= "main">
+		<section id="pod" data-type="background" data-speed="10" class="parallax pages">
+			<article>
+				<table class ='tprograms'><tr>
+								<td >
+				<div id="vertflex" class="flexslider">
+					  <ul class="slides">
+					    <li>
+					      <img src="images/v1.JPG" />
+					    </li>
+					    <li>
+					      <img src="images/v2.JPG" />
+					    </li>
+					    <li>
+					      <img src="images/v3.JPG" />
+					    </li>
+					  </ul>
+					</div>
+				</td>
+				<td>
+				<h1>Parents-on-Duty</h1>
+					<p>I love PODs.</p>
+
+				</td>
+	</tr></table>
+			</article>
+		</section>
+
+		<section id='pod2' data-type="background" data-speed="10" class="parallax pages">
+			<article>
+				<div id = "finaid" class="overlay">
+					<h1>PODS ARE SO COOL</h1>
+					<p>WAHOO</p>
+				</div>
+			</article>
+		</section>
+</div>
+
+	</body>
+</html>
