204b28ecaf20462b72cb934f0c8862fa79a19208
Rachel Wang <rswang@mit.edu>
2014-01-20 01:31:12 -0500

    Add navigation and sections to Forms page

--- forms.php
+++ forms.php
@@ -7,17 +7,63 @@
 		<?php include 'nav.php'; ?>
 
 	<div class='main'>
-		<p>I would like to...</p>
-        <a href='#apply'><input type = "submit" value="Apply" id = "formsubmit"></a>
-        <a href='#forms'><input type = "submit" value="Download Forms" id = "formsubmit"></a>
-        <a href='#status'><input type = "submit" value="Check my status" id = "formsubmit"></a>
 
-        <section id='apply'>
-        	
+	<div class='main'>
+		<section id="formsnav" data-type="background" data-speed="10" class="pages">
+			<article>
+				<table class="center">
+					<tr>
+					<td>
+					<a href="#apply">
+						<div class="hover2 ">
+		                    <img src="images/bg4.jpg" />
+		                    <div class="mask">
+		                        <h2>Apply</h2>
+		                    </div>
+		                </div>
+		            </a>
+		            </td>
+		            <td>
+					<a href="#forms">
+						<div class="hover2 ">
+		                    <img src="images/bg4.jpg" />
+		                    <div class="mask">
+		                        <h2>Download forms</h2>
+		                    </div>
+		                </div>
+		            </a>
+		        </td>
+		        <td>
+					<a href="#status">
+						<div class="hover2 ">
+		                    <img src="images/bg4.jpg" />
+		                    <div class="mask">
+		                        <h2>Check status</h2>
+		                    </div>
+		                </div>
+		            </a>
+		        </td>
+		        </tr>
+		        </table>
+	        </article>
+	    </section>
+		<section id="apply" data-type="background" data-speed="10" class="parallax pages">
+			<article>
+
+        	</article>
         </section>
+		<section id="forms" data-type="background" data-speed="10" class="parallax pages">
+			<article>
+        <a href='#apply'><input type = "submit" value="Health Form A" id = "formsubmit"></a>
+        <a href='#forms'><input type = "submit" value="Health Form B" id = "formsubmit"></a>
+        <a href='#status'><input type = "submit" value="Liability Waiver" id = "formsubmit"></a>
+        	</article>
+        </section>
+		<section id="status" data-type="background" data-speed="10" class="parallax pages">
+			<article>
 
-
-
+        	</article>
+        </section>
 
 	</div>
 
